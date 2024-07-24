e807f5755f1fa52b6453bade93308baed9217cfe
junhan <x19967963@gmail.com>
2024-07-24 21:47:44 +0800

    初始化springboot

---
+++ src/main/java/com/junhan/springbootyamalulu/SpringbootYamaLuLuApplication.java
@@ -0,0 +1,13 @@
+package com.junhan.springbootyamalulu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootYamaLuLuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootYamaLuLuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/junhan/springbootyamalulu/SpringbootYamaLuLuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junhan.springbootyamalulu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootYamaLuLuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
